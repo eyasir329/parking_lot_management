658023edde4cbacc73a9c463636552533438ace7
eyasir329 <eyasir329@gmail.com>
2023-07-13 04:53:21 +0600

    main start

---
+++ Main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//parkingLotInitialize
+//findAvailableSpace
+//vehiclePark
+//freeParkingSpace
+//plotNo
+//location
+//avail
+//parkinginfo\
+//linkedlist
+//queue
+int main(){
+
+    return 0;
+}
